c16c7155fa44883f7a661002d05b79fe2c49eedf
IMAR ALEXANDER AVILA LOPEZ <iavila@ad.elektra.com.mx>
2017-11-16 14:23:44 -0600

    save

--- public/js/app.js
+++ public/js/app.js
@@ -32,9 +32,7 @@ var PushProductApp = React.createClass({
 			sku: document.getElementById('skuEl').value
 		};
 		socket.emit('send:product', JSON.stringify(promoProduct));
-		savePromo();
-	},
-	savePromo: function savePromo() {
+
 		$.ajax({
 			method: 'GET',
 			url: "https://api.vtex.com/elektra/dataentities/OR/search/?_fields=id,sku,last",
