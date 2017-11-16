89c2bb7bd739a00150aa2d833ec5140e665fe29c
IMAR ALEXANDER AVILA LOPEZ <iavila@ad.elektra.com.mx>
2017-11-16 13:53:00 -0600

    commit

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,7 @@
 'use strict';
 
 var React = require('react');
-
+var $ = require('jquery');
 var socket = io.connect();
 
 var PushProductApp = React.createClass({
@@ -13,26 +13,50 @@ var PushProductApp = React.createClass({
 	},
 	sendPromo() {
 		var promoProduct = {
-			sku: document.getElementById('skuEl').value,
-			img: document.getElementById('imgEl').value,
-			precio: document.getElementById('precioEl').value
+			sku: document.getElementById('skuEl').value
 		};
 		socket.emit('send:product', JSON.stringify(promoProduct));
-	},
+		$.ajax({
+      method: 'GET',
+      url: "http://api.vtex.com/elektra/dataentities/OR/search/?_fields=id,sku,last",
+      success: function(data, textStatus, xhr){
+        if (data.length == 0) {
+					var data = {
+						sku: document.getElementById('skuEl').value,
+						last: true
+					}
 
+					$.ajax({
+						method: 'PATCH',
+						url: "http://api.vtex.com/elektra/dataentities/OR/documents/",
+						data: JSON.stringify(data),
+						success: function(data, textStatus, xhr){
+						}
+					});
+        } else {
+					var data = {
+						sku: document.getElementById('skuEl').value,
+						last: true
+					}
+					$.ajax({
+						method: 'PUT',
+						url: "http://api.vtex.com/elektra/dataentities/OR/documents/" + action.id,
+						data: JSON.stringify(data),
+						processData: false,
+						success: function(data){
+						}
+					});
+        }
+        AppStore.emitChange();
+      }
+    });
+	},
 	render() {
 		return (
 			<div>
 				<label>SKU:</label>
 				<input id="skuEl" type="text" />
-				<br></br>
-				<br></br>
-				<label>Img:</label>
-				<input id="imgEl" type="text" />
-				<br></br>
-				<br></br>
-				<label>Precio:</label>
-				<input id="precioEl" type="text" />
+
 				<br></br>
 				<br></br>
 				<input type="button" value="Send Promo" onClick={() => this.sendPromo()}/>
